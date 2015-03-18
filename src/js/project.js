d84b1e77784525bab291d4c737f28f35ec309310
Øyvind Nordbø <oyvinor@gmail.com>
2015-03-18 23:51:32 +0100

    Update workflow page, minor updates

--- src/js/project.js
+++ src/js/project.js
@@ -10,30 +10,24 @@ var projectList = [
 	"spotify"
 ];
 
-//Index and randomize projectList
+//Index function
 var randomProject = function () {
 	//Count objects in projectList array
 	var indexProject = Math.floor(Math.random()*projectList.length)
 	//Retrieve an object based on random index
-	var url = domain + projectList[indexProject];
+	var url = domain+projectList[indexProject];
 	//Return chosen object
 	return url.toString();
 }
 
 //Reload page with chosen object
 var loadRandomProject = function () {
-	//Get the currently active url
-	var checkCurrentURL = function () {
-		//Get active URL
-		var projectLink = document.URL;
-		//Splice active URL
-		var project = url.splice(projectLink, url.length - "http://beta.oyvind.co/");		
-		//Exclude current URL from projectIndex
-		if (project == activeProject) {
-			randomProject();
-		} else {
-			//Serve object
-			window.location.href = randomProject();
-		}
-	}
+	window.location.href = randomProject();
 }
+
+/*
+	//If you want on siteload; call loadRandomProject()
+	window.onload = function(){
+		loadRandomProject();
+	};
+*/
