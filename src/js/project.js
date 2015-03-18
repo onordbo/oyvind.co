594deee6db7bcd81082bbaf673add92054358aeb
Øyvind Nordbø <oyvinor@gmail.com>
2015-03-18 18:19:23 +0100

    Update project.js

--- src/js/project.js
+++ src/js/project.js
@@ -6,7 +6,6 @@ var projectList = [
 	"dated",
 	"lava",
 	"geolarm",
-	"workflow",
 	"shapr",
 	"spotify"
 ];
@@ -21,19 +20,19 @@ var randomProject = function () {
 	return url.toString();
 }
 
-//Get the currently active url
-var checkCurrentURL = function () {
-	//Store currently active link
-	var activeProject = document.URL;
-	//Strip "http://oyvind.co/" from link
-	var project = activeProject - "http://beta.oyvind.co/"
-	//Exclude current URL from projectIndex
-	if (project == activeProject) {
-		randomProject;
-	}
-}
-
 //Reload page with chosen object
 var loadRandomProject = function () {
+	//Get the currently active url
+	var checkCurrentURL = function () {
+		//Store currently active link
+		var activeProject = document.URL;
+		//Strip "http://oyvind.co/" from link
+		var project = activeProject - "http://beta.oyvind.co/"
+		//Exclude current URL from projectIndex
+		if (project == activeProject) {
+			randomProject();
+		}
+	}
+	//Serve object
 	window.location.href = randomProject();
 }
