39e20371ee56a0c72e0f403d49c94126f2eb691d
Øyvind Nordbø <oyvinor@gmail.com>
2015-03-18 13:20:41 +0100

    Minor copy updates, add URL checker to project.js

--- src/js/project.js
+++ src/js/project.js
@@ -7,10 +7,11 @@ var projectList = [
 	"lava",
 	"geolarm",
 	"workflow",
+	"shapr",
 	"spotify"
 ];
 
-//Index function
+//Index and randomize projectList
 var randomProject = function () {
 	//Count objects in projectList array
 	var indexProject = Math.floor(Math.random()*projectList.length)
@@ -18,6 +19,18 @@ var randomProject = function () {
 	var url = domain + projectList[indexProject];
 	//Return chosen object
 	return url.toString();
+}
+
+//Get the currently active url
+var checkCurrentURL = function () {
+	//Store currently active link
+	var getProject = document.URL;
+	//Strip "http://oyvind.co/" from link
+	var currentProject = getProject - "http://beta.oyvind.co/"
+	//Exclude current URL from projectIndex
+	if (currentProject == getProject) {
+		randomProject;
+	}
 }
 
 //Reload page with chosen object
